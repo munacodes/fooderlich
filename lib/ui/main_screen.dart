cfc7d32dd0dbbcf0b241927062035c36d79c47bb
Muna <110025277+munacodes@users.noreply.github.com>
2022-11-29 17:37:45 +0100

    updates

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -7,6 +7,7 @@ import 'myrecipes/my_recipes_list.dart';
 import 'recipes/recipe_list.dart';
 import 'shopping/shopping_list.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -18,26 +19,56 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
   List<Widget> pageList = <Widget>[];
-  // TODO: Add index key
 
+  // index key
+  static const String prefSelectedIndexKey = 'selectedIndex';
+// That is the constant you will use for the selected index preference key.
   @override
   void initState() {
     super.initState();
     pageList.add(const RecipeList());
     pageList.add(const MyRecipesList());
     pageList.add(const ShoppingList());
-    // TODO: Call getCurrentIndex
+    // Calls getCurrentIndex
+    getCurrentIndex();
+    // That will retrieve the currently-selected index when the page is loaded.
   }
 
-  // TODO: Add saveCurrentIndex
+  // SaveCurrentIndex
+  void saveCurrentIndex() async {
+    final prefs = await SharedPreferences.getInstance();
 
-  // TODO: Add getCurrentIndex
+    prefs.setInt(prefSelectedIndexKey, _selectedIndex);
+  }
+// Here, you:
+// 1. Use the await keyword to wait for an instance of the shared preference plugin.
+// 2. Save the selected index as an integer.
+
+  // GetCurrentIndex
+  void getCurrentIndex() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    if (prefs.containsKey(prefSelectedIndexKey)) {
+      setState(() {
+        final index = prefs.getInt(prefSelectedIndexKey);
+        if (index != null) {
+          _selectedIndex = index;
+        }
+      });
+    }
+    // With this code, you:
+// 1. Use the await keyword to wait for an instance of the shared preference plugin.
+// 2. Check if a preference for your current index already exists.
+// 3. Get the current index and update the state accordingly
+  }
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
-    // TODO: Call saveCurrentIndex
+    // Calls saveCurrentIndex
+    saveCurrentIndex();
+    // This saves the current index every time the user selects a different tab.
   }
 
   @override
