834bd52320eb4dc0aacb1476ca752a239002cd4b
Muna <110025277+munacodes@users.noreply.github.com>
2022-12-01 13:53:45 +0100

    updates

--- ios/network/recipe_model.dart
+++ ios/network/recipe_model.dart
@@ -4,9 +4,14 @@ part 'recipe_model.g.dart';
 @JsonSerializable()
 class APIRecipeQuery {
   // TODO: Add APIRecipeQuery.fromJson
+  factory APIRecipeQuery.fromJson(Map<String, dynamic> json) =>
+      _$APIRecipeQueryFromJson(json);
+  Map<String, dynamic> toJson() => $APIRecipeQueryToJson(this);
+  // TODO: Add fields here
+  // TODO: Add APIRecipeQuery constructor
 }
 
 // TODO: Add @JsonSerializable() class APIHits
 // TODO: Add @JsonSerializable() class APIRecipe
 // TODO: Add @JsonSerializable() class APIIngredients
-/// Creates a new [JsonSerializable] instance.
+
