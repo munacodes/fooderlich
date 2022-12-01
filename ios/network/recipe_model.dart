e3ebf3b71e70813e1b762b5c0428c1465d71f031
Muna <110025277+munacodes@users.noreply.github.com>
2022-12-01 13:05:43 +0100

    serialization with json

---
+++ ios/network/recipe_model.dart
@@ -0,0 +1,12 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'recipe_model.g.dart';
+
+@JsonSerializable()
+class APIRecipeQuery {
+  // TODO: Add APIRecipeQuery.fromJson
+}
+
+// TODO: Add @JsonSerializable() class APIHits
+// TODO: Add @JsonSerializable() class APIRecipe
+// TODO: Add @JsonSerializable() class APIIngredients
+/// Creates a new [JsonSerializable] instance.
